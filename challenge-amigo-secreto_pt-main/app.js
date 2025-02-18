574c15b05af8dd2d165f3736dd1edfae899e3e31
Vitoria Kaupp <vikaupp@gmail.com>
2025-02-18 16:55:17 -0300

    adicionando código JS

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,37 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value;
+
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+    
+    amigos.push(nome); 
+    atualizarLista();  
+    input.value = "";  
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+
+    amigos.forEach(function(nome) {
+        lista.innerHTML += "<li>" + nome + "</li>"; 
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length < 1) {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+    
+    let indiceSorteado = parseInt(Math.random() * amigos.length); 
+    let nomeSorteado = amigos[indiceSorteado];
+    
+    document.getElementById("resultado").innerHTML = "<li> " + nomeSorteado + " foi sorteado! </li>";
+}
+
